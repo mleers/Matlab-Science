fa735aa1428a9b88e70c5977d42a6d56f85f0704
mleers <mleers4@gmail.com>
2018-11-18 14:40:45 -0800

    Delete img_loader.m

--- img_loader.m
+++
@@ -1,6 +0,0 @@
-function im = img_loader(tFile)
-    % FUNCTION: img_loader
-    % RETURNS: the array of JPG image data
-    % INPUTS: tFile = file path to JPG image file
-    im = imread(tFile);
-end 
